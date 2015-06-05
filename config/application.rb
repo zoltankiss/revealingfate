072bda5aa9a49017513953518aaff4ff585cee9e
Zoltan Kiss <zkiss@ackmanndickenson.com>
2015-06-05 02:12:15 -0500

    Remove ActiveRecord
    Conflicts:
    	Gemfile.lock

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+%w(
+  action_controller
+  action_view
+  action_mailer
+  active_job
+  rails/test_unit
+  sprockets
+).each do |framework|
+  begin
+    require "#{framework}/railtie"
+  rescue LoadError
+  end
+end
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +34,5 @@ module RevealingFate
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -11,5 +11,7 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
+
+  # commented this out - we've removed activerecord
+  # ActiveRecord::Base.establish_connection
 end
