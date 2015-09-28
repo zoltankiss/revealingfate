9c8e655055b5608e2ccce97f60bd4d0f64d91c1a
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2015-09-27 22:19:23 -0500

    first draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get '/team' => 'pages#team'
+  get '/reading_services' => 'pages#reading_services'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
