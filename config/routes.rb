e572dce91f1643e6adee4b301e658748b875f15b
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2015-10-26 23:30:23 -0500

    'Contact Us' tab

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/team' => 'pages#team'
   get '/reading_services' => 'pages#reading_services'
   get '/newsletter_signup' => 'pages#newsletter_signup'
+  get '/contact_us' => 'pages#contact_us'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
