1e117b2d2fa8249c5fc24d14bfbdf3b715e7a491
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2016-03-05 21:43:07 -0600

    color code reading types on admin panel

--- config/initializers/reading_services_helper.rb
+++ config/initializers/reading_services_helper.rb
@@ -17,4 +17,17 @@ class ReadingServicesHelper
   def self.tier_to_int(tier)
     TIERS.index tier.capitalize
   end
+
+  def self.color(nth_tier)
+    case nth_tier
+    when 0
+      'green'
+    when 1
+      'blue'
+    when 2
+      'red'
+    when 3
+      'black'
+    end
+  end
 end
