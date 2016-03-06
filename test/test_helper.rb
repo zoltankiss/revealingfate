91e0d4d0de875647b25d5450d407beadde0a4413
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2016-03-06 14:48:25 -0600

    use poltergeist for javascript integration tests (faster)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require 'capybara/poltergeist'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -9,6 +10,8 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+Capybara.javascript_driver = :poltergeist
 
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
