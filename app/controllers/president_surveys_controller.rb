f2b304b3da11f2f3ee4c364a60e87948f5188be4
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2016-04-19 23:24:15 -0500

    updates to mystery reading #1

--- app/controllers/president_surveys_controller.rb
+++ app/controllers/president_surveys_controller.rb
@@ -2,6 +2,6 @@ class PresidentSurveysController < ApplicationController
   def create
     cookies[:president_survey] = 'taken'
     PresidentSurvey.create! choice: params[:president_survey][:choice]
-    redirect_to '/unknown_candidate_1'
+    redirect_to '/unknown_candidate_1#vote'
   end
 end
