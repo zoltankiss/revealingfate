0c133b142a33d3b68f068a813d3ffb0d6407f618
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2016-05-19 23:23:51 -0500

    fix prez survey % bug

--- app/models/president_survey.rb
+++ app/models/president_survey.rb
@@ -7,11 +7,11 @@ class PresidentSurvey < ActiveRecord::Base
     dt = PresidentSurvey.where(choice: 'Donald Trump', name: name).count
     bern = PresidentSurvey.where(choice: 'Bernie Sanders', name: name).count
     k = PresidentSurvey.where(choice: 'John Kasisch', name: name).count
-    "Clinton: #{pct h} Sanders: #{pct bern} Trump: #{pct dt} Cruz: #{pct ted} Kasisch #{pct k}"
+    "Clinton: #{pct h, name} Sanders: #{pct bern, name} Trump: #{pct dt, name} Cruz: #{pct ted, name} Kasisch #{pct(k, name)}"
   end
 
-  def self.pct n
-    count = PresidentSurvey.count.to_f
+  def self.pct(n, name)
+    count = PresidentSurvey.where(name: name).count.to_f
     return '0%' if count == 0
     return (n/count * 100).round.to_s + '%'
   end
