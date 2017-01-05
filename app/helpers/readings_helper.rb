2f1d313ec0d2320c8cc9ac727e9aef1a3121f2b9
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2017-01-04 21:57:14 -0600

    add 'update reading' buttons next to all form elements

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,2 +1,9 @@
 module ReadingsHelper
+  def readings_text_field(form, str)
+    form.text_field(str) + form.submit
+  end
+
+  def readings_text_area(form, str, opts = {})
+    form.text_area(str, opts) + form.submit
+  end
 end
