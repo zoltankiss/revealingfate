cccdc941fcf830eef62f7cfe04358291c9d92746
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2017-01-04 22:15:42 -0600

    make text-area, text-field divs linkable

--- app/helpers/readings_helper.rb
+++ app/helpers/readings_helper.rb
@@ -1,9 +1,13 @@
 module ReadingsHelper
   def readings_text_field(form, str)
-    form.text_field(str) + form.submit
+    tag("div", { id: str }, true) +
+      form.text_field(str) + form.submit +
+        tag("div", nil, false)
   end
 
   def readings_text_area(form, str, opts = {})
-    form.text_area(str, opts) + form.submit
+    tag("div", { id: str }, true) +
+      form.text_area(str, opts) + form.submit +
+        tag("div", nil, false)
   end
 end
